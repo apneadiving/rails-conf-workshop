d5aabbf0acf6b0c376a0b9ebb3ba56224fbd79b8
Benjamin Roth <benjamin@rubyist.fr>
2017-04-25 00:37:37 +0200

    accept invitation service

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,12 +4,9 @@ class InvitationsController < ApplicationController
     if invitation.accepted?
       render json: { errors: ['Invitation already accepted'] }, status: 422
     else
-      create_service = ::Services::User::CreateFromInvitation.new(invitation)
-      create_service.call
-      invitation.accepted = true
-      invitation.save
-      Services::User::Credit.new(user: invitation.inviter, cents: 500, source: invitation).call
-      render json: { user: create_service.user }
+      service = Services::Invitation::Accept.new(invitation)
+      service.call
+      render json: { user: service.user }
     end
   end
 
--- app/services/services/invitation/accept.rb
+++ app/services/services/invitation/accept.rb
@@ -9,10 +9,20 @@ module Services
       end
 
       def call
-        # would host code from invitations_controller
+        create_service = ::Services::User::CreateFromInvitation.new(invitation)
+        create_service.call
+        invitation.accepted = true
+        invitation.save
+        Services::User::Credit.new(user: invitation.inviter, cents: REFERRER_FEE_IN_CENTS, source: invitation).call
+      end
+
+      def user
+        create_service.user
       end
 
       private
+
+      REFERRER_FEE_IN_CENTS = 500
 
       attr_reader :invitation
     end
